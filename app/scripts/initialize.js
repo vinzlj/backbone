ece531a2f2bbd23d20cf250780cca815a9fd3d4e
Vincent Le Jeune <vincent.lejeune@wincomparator.com>
2015-06-10 16:32:56 +0200

    WTF

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -1,3 +1,4 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
+// Backbone ne tente pas un require('jquery'), il exige le global.  Bizarre…
 Backbone.$ = $;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 require('initialize');
+
+var $ = require('jquery');
+
+var coucou = require('coucou');
+var yo = coucou.hello();
+
+$('#content').html(yo);
