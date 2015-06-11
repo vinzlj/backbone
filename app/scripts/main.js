3546898f0c0c00d007a739b3b1dc016ab440b0c0
Vincent Le Jeune <vincent.lejeune@wincomparator.com>
2015-06-11 19:03:57 +0200

    Mon premier commit en français

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,18 @@
-require('initialize');
+require('scripts/initialize');
 
-var $ = require('jquery');
-
-var coucou = require('coucou');
+var coucou = require('scripts/components/coucou');
 var yo = coucou.hello();
 
 $('#content').html(yo);
+
+var App = {
+    init: function init() {
+        console.log('App initialized.');
+        console.log(Backbone.View);
+    }
+};
+
+console.log(Backbone.View);
+
+module.exports = App;
+window.App = App;
--- brunch-config.coffee
+++ brunch-config.coffee
@@ -1,4 +1,14 @@
 module.exports = config:
   files:
-    javascripts: joinTo: 'app.js'
-    stylesheets: joinTo: 'app.css'
+    javascripts:
+      joinTo: 'app.js'
+      order:
+        before: [
+          'vendor/scripts/jquery.js',
+          'vendor/scripts/underscore.js',
+          'vendor/scripts/backbone.js'
+        ]
+    stylesheets:
+      joinTo: 'app.css'
+    templates:
+      joinTo: 'app.js'
